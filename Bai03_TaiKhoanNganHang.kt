033965399817e0a44164d1ef453e21c056c7ee20
PhucLH-dev <phuclh8604@gmail.com>
2026-09-24 15:18:23 +0700

    Buoi02 Bai03 tai khoan ngan hang

---
+++ Bai03_TaiKhoanNganHang.kt
@@ -0,0 +1,10 @@
+// Lê Hoàng Phúc - 25810034
+fun main(){
+    val soDuBanDau: Double = 4_000_000.0
+    var soDuThayDoi: Double = soDuBanDau
+
+    soDuThayDoi +=  1_000_000.0
+    println("Số dư tài khoàn sau khi thêm 1.000.000VNĐ: $soDuThayDoi")
+    soDuThayDoi -= 1_500_000.0
+    println("Số dư tài khoản sau khi rút 1.500.000 VNĐ: $soDuThayDoi")
+}
