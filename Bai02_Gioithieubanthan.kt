4d00e2162be94d596d97210c10f264120af9f5de
PhucLH-dev <phuclh8604@gmail.com>
2026-09-24 15:18:23 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_Gioithieubanthan.kt
@@ -0,0 +1,8 @@
+// Lê Hoàng Phúc - 25810034
+fun main(){
+    val hoTen: String = "Lê Hoàng Phúc"
+    val tuoi: Int = 22
+    val namSinh: Int = 2004
+
+    println("Họ và tên: $hoTen, $tuoi tuổi, Sinh năm: $namSinh")
+}
